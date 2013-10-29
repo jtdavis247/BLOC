36d41d9ed1505c1049494b5adfa729685a752a24
Jeff Davis <jtdavis247>
2013-10-29 02:46:23 -0600

    Finished design and static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Learn Game Development Now | Home")
+    end
+  end
+end
