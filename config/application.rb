907899fb0d4a09435db5d714dfeaa3c59fcea2af
Jeff Davis <jtdavis247>
2013-10-31 11:47:49 -0600

    assets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module BLOC
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.precompile += ['style.css']
   end
+
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,6 @@ BLOC::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  
 end
